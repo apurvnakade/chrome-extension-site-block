b8e7a807059128c7c887481430763065707c19df
Apurva Nakade <apurvnakade@gmail.com>
2020-12-09 10:05:27 -0500

    update list of websites

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(function(details) { 
 		return {cancel: true}; 
 	},
-	{urls: ["*://*.reddit.com/*"]},
+	{urls: [
+		"*://*.reddit.com/*",
+		"*://*.nytimes.com/*",
+		"*://*.theatlantic.com/*",
+		"*://*.theguardian.com/*",
+		"*://*.foxnews.com/*",
+		"*://*.news.google.co.in/*",
+		"*://*.news.google.com/*",
+		]},
 	["blocking"]);
